ca6c669928d624aed6dcbcde70d1f71ebf1b9f9d
Oliver Mensah <olivermensah96@gmail.com>
2019-02-11 05:23:52 +0000

    added

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -24,7 +24,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 
-// import "./Dashboard.css"
+import BarChart from '../../components/Chart/Chart'
 
 
 const styles = theme => ({
@@ -36,21 +36,25 @@ const styles = theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  row:{
+    marginTop: '20px'
+  }
 });
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(eventName, totalParticipants, numberOfMales, numberOfFemales) {
   id += 1;
-  return { id, name, calories, fat, carbs, protein };
+  return { id, eventName, totalParticipants, numberOfMales, numberOfFemales};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Developer Jobs 101', 159, 82, 68),
+  createData('Public Speaking for Developers', 220, 94, 126),
+  createData('React Workshop', 84, 59, 25),
+  createData('Campus Roadshow', 42, 30, 12),
+
 ];
+const totalParticipants = [159, 220, 84, 42]
 
 
 class Dashboard extends React.Component {
@@ -74,23 +78,21 @@ class Dashboard extends React.Component {
             <Table className={classes.table}>
             <TableHead>
               <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat (g)</TableCell>
-              <TableCell align="right">Carbs (g)</TableCell>
-              <TableCell align="right">Protein (g)</TableCell>
+              <TableCell>Upcoming Events</TableCell>
+              <TableCell align="right">Registered participants</TableCell>
+              <TableCell align="right">Number of Males</TableCell>
+              <TableCell align="right">Number of Females</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map(row => (
               <TableRow key={row.id}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.eventName}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.totalParticipants}</TableCell>
+                <TableCell align="right">{row.numberOfMales}</TableCell>
+                <TableCell align="right">{row.numberOfFemales}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -178,6 +180,7 @@ class Dashboard extends React.Component {
                   <Paper className={classes.paper}>
                     {sendMessage}
                     {events}
+                    <BarChart className={classes.marginTop} totalParticipants={totalParticipants}/>
                   </Paper>
                 </Grid>
               </Grid>
